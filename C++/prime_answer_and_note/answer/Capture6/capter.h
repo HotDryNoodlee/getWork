c636818ef655964b992024b8d379fb26425c5131
lancer <l332142528@qq.com>
2024-08-20 19:52:47 +0800

    delete all

--- C++/prime_answer_and_note/answer/Capture6/capter.h
+++
@@ -1,8 +0,0 @@
-#ifndef CAPTER_H_
-#define CAPTER_H_
-
-int fact(int );
-double myABS(double );
-double myABS2(double );
-void myswap(int *p, int *q);
-#endif
--- C++/prime_answer_and_note/answer/Capture6/myswap.cpp
+++
@@ -1,8 +0,0 @@
-#include "capter.h"
-using namespace std;
-
-void myswap(int *p, int *q){
-    int temp = *p;
-    *p = *q;
-    *q =temp;
-}
